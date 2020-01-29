d589d00f7fd33fe0601efead97d5d14e1b7652ee
Mike Zazaian <mike@functionhaus.com>
2020-01-29 15:30:13 -0500

    add simple integration test for csv upload

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "loading the index" do
+    get "/"
+    assert_response :success
+  end
+
+  test "posting a CSV file" do
+    get "/"
+
+    # post the login and follow through to the home page
+    csv_fixture = fixture_file_upload('test/support/eq_page_outline_v3.csv', 'text/csv')
+    post "/pages/parse_csv", params: {paths_file: csv_fixture}
+    assert_response :success
+    assert_equal "/pages/parse_csv", path
+  end
 end
